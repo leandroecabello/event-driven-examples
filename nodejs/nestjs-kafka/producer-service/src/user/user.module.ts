b6e17f0c7b04a2eb39b6c0dfcd710e6b754d58fd
Leandro Cabello <lcabello@gmail.com>
2025-03-22 03:31:47 -0300

    Add user module

---
+++ nodejs/nestjs-kafka/producer-service/src/user/user.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'user-producer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
